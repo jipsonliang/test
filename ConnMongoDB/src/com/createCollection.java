eddcce5606c2980a6678dd120f30346820ea66a0
Administrator <Administrator@LAPTOP-L9D00CD4>
2019-01-11 21:24:20 +0800

    第一次提交

---
+++ ConnMongoDB/src/com/createCollection.java
@@ -0,0 +1,25 @@
+package com;
+
+
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+public class createCollection{
+   public static void main( String args[] ){
+      try{   
+      // 连接到 mongodb 服务
+      MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+         
+       
+      // 连接到数据库
+      MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");  
+      System.out.println("Connect to database successfully");
+      mongoDatabase.createCollection("test11");
+      System.out.println("集合创建成功");
+        
+      }catch(Exception e){
+        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+     }
+   }
+}
